2432325cee61f46a01e18ec15c5c63f262a305ab
AdrianoMendesBorges <95090401+AdrianoMendesBorges@users.noreply.github.com>
2022-01-02 00:57:21 -0300

    Add files via upload

---
+++ eladeb_login/conexaoBancoDados.php
@@ -0,0 +1,16 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$my_db = "eladeb";
+$port = "3307";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $my_db, $port);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
