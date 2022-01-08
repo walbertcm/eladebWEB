79188111241167f5e64e3a942da9530047ee9cec
walbertcm <walbertcm@gmail.com>
2022-01-08 18:27:47 -0300

    009

---
+++ js/eladeb.js
@@ -0,0 +1,12 @@
+
+//Funcao Criar nova Janela
+function novaJanela(){
+    var nj = window.open('avaliacaoB.html', '', 'width=800, height=600');
+    //nj.document.write(<p>NOva Janela</p>); 
+}
+
+
+//Função exibir Imagens
+function showImages(src, width, height){
+
+}
