8bda8480f511713ac4af1d18bfe3c10a40b7c8fb
Walbert Monteiro <walbertcm@gmail.com>
2022-01-27 07:21:47 -0300

    27.01-07:21

--- indexBack.php
+++ indexBack.php
@@ -1,34 +1,70 @@
 <?php
  //Conexão do banco de dados online
  include('conexaoDataBaseV2.php');
+
+ //Recebe elementos POST
+ $email = "administrador01@mail.com";//$_POST['email'];
+ $pass = "123456";//$_POST['pass'];
+
 //Seleção no banco de dados
- $sqlA = "SELECT * FROM `loginSistema`";
+$sqlA = "SELECT * FROM `loginSistema` WHERE `usuario` = '$email' AND `senha` = '$pass' ";
+
 //Query
  $query = mysqli_query($conn, $sqlA);
+
  //Tratamento da resposta
  while($dados = mysqli_fetch_assoc($query)){
     $id=$dados["id"];
     $nome=$dados["usuario"];
-    $tipo=$dados['senha'];
- 
-    switch($tipo!=0){
+    $tipo=$dados['tipo'];
+            echo $id;
+            echo $nome;
+            echo $tipo;
+    switch($tipo){
         case 1:
             header("Location: useradministrador/principaladministrador.php?email=$nome");
+            echo $id;
+            echo $nome;
+            echo $tipo;
             break;
         case 2:
-            header("Location: usererapeuta/principalterapeuta.php?email=$nome");
+            header("Location: userterapeuta/principalterapeuta.php?email=$nome");
+            echo $id;
+            echo $nome;
+            echo $tipo;
             break;
         case 3:
-            header("Location: userpaciente/principaciente.php?email=$nome");
+            header("Location: userpaciente/principalpaciente.php?email=$nome");
+            echo $id;
+            echo $nome;
+            echo $tipo;
             break;
     }
     
-    echo $id;
-    echo $nome;
-    echo $tipo;
+    
  }
+ ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?
+    while($dados = mysqli_fetch_assoc($query)){
+        $id=$dados["id"];
+        $nome=$dados["usuario"];
+        $tipo=$dados['tipo'];
+                echo $id;
+                echo $nome;
+                echo $tipo;
+    }
+    ?>
+</body>
+</html>
 
 
 
-
-?>
