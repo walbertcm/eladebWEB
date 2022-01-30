b32be1f78f3fe6c933d52cd3c597cc040d38e281
Walbert Monteiro <walbertcm@gmail.com>
2022-01-30 15:49:14 -0300

    30.01-15:49

---
+++ userterapeuta/cenarioCadastrar.php
@@ -0,0 +1,16 @@
+<?php
+
+//Metodo para iniciar a sessao
+session_start();
+//Avalia se a sessao tem valores, foi definida, caso nao retorna o user para o login
+if(!isset($_SESSION["emailUsuario"]) AND !isset($_SESSION["idUsuarioLogin"])){
+    header("Location: ../index.html");
+    die();
+}else{
+    $emailUsuario = $_SESSION["emailUsuario"];
+    $idUsuarioLogin = $_SESSION["idUsuarioLogin"];
+}
+
+include('../controller/conexaoDataBaseV2.php');
+
+?>
